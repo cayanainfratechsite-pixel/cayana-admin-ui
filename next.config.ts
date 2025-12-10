d92329d2854dc0954df9dac71f22f724772d3cfe
sujit1515 <sujitpattnaik41@gmail.com>
2025-12-11 01:05:10 +0530

    innitial commit

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,29 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: false,
+//   images: {
+//     domains: [
+//       "images.unsplash.com",
+//       "media.istockphoto.com",
+//       "images.ctfassets.net",
+//       "example.com",
+//       "artgallery.yale.edu",
+//       "www.photocrati.com",
+//       "localhost",
+//       "145.223.23.134",
+//       "api.cayana.co.in",
+//     ],
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -12,12 +38,10 @@ const nextConfig: NextConfig = {
       "www.photocrati.com",
       "localhost",
       "145.223.23.134",
-      "api.cayana.co.in",
+      "api.cayana.co.in"
     ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
 export default nextConfig;
+
