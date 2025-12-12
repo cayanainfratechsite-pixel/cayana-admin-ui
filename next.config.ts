a9413b47a2611219a3c55e9fd7566d67632c74bb
sujit1515 <sujitpattnaik41@gmail.com>
2025-12-12 11:22:42 +0530

    innitial commit

--- next.config.ts
+++ next.config.ts
@@ -24,89 +24,24 @@
 
 
 
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   reactStrictMode: false,
-//   images: {
-//     domains: [
-//       "images.unsplash.com",
-//       "media.istockphoto.com",
-//       "images.ctfassets.net",
-//       "example.com",
-//       "artgallery.yale.edu",
-//       "www.photocrati.com",
-//       "localhost",
-//       "145.223.23.134",
-//       "api.cayana.co.in"
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "backend.cayana.co.in",
-        pathname: "/public/**",
-      },
-      {
-        protocol: "https",
-        hostname: "api.cayana.co.in",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "media.istockphoto.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.ctfassets.net",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "example.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "artgallery.yale.edu",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.photocrati.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "145.223.23.134",
-        pathname: "/**",
-      }
+    domains: [
+      "images.unsplash.com",
+      "media.istockphoto.com",
+      "images.ctfassets.net",
+      "example.com",
+      "artgallery.yale.edu",
+      "www.photocrati.com",
+      "localhost",
+      "145.223.23.134",
+      "api.cayana.co.in"
     ],
   },
 };
 
 export default nextConfig;
-
 
